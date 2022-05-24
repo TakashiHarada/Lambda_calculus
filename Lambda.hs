8eb49cd2b5c299fbb37d22f25e5b21dceb233529
TakashiHarada <harada.takashi@kochi-tech.ac.jp>
2022-05-24 16:14:39 +0900

    implement beta reduction

--- Lambda.hs
+++ Lambda.hs
@@ -1,3 +1,5 @@
+module Lambda where
+
 import Data.List
 
 -- Lambda Term
@@ -39,17 +41,6 @@ vars m = (nub . vars') m
     vars' (App m n) = (vars' m) ++ (vars' n)
     vars' (Abs x m) = x : (vars' m)
 
--- free variables in M
--- fv :: LT -> [Var]
--- fv m = nub (fv' m [])
---   where
---     fv' :: LT -> [Var] -> [Var]
---     fv' (A x) zs
---       | elem x zs = []
---       | otherwise = [x]
---     fv' (App m n) zs = (fv' m zs) ++ (fv' n zs)
---     fv' (Abs x m) zs = (fv' m (x:zs)) 
-
 fv :: LT -> [Var]
 fv t = (nub . fv') t
 
@@ -83,63 +74,32 @@ sub (Abs y p) n x
   | otherwise = Abs z (sub (sub p (A z) y) n x)
   where z = fstvnin (App n p)
 
+getBetaRedex :: LT -> [LT]
+getBetaRedex (A v) = []
+getBetaRedex (App (A v) n) = getBetaRedex n
+getBetaRedex (App (Abs x p) n) = (App (Abs x p) n) : (getBetaRedex p) ++ (getBetaRedex n)
+getBetaRedex (App m n) = (getBetaRedex m) ++ getBetaRedex n
+getBetaRedex (Abs x p) = getBetaRedex p
 
+-- alphaConv :: LT -> Var -> LT
+-- alphaConv (A v) _ = A v
+-- alphaConv (App m n) x = App (alphaConv m x) (alphaConv n x)
+-- alphaConv (Abs x m) y
+--   | not (elem y (fv m)) = Abs y (sub m (A y) x)
+--   | otherwise = Abs x m
 
+-- beta contracts the left most beta redex
+lmbContruct :: LT -> LT
+lmbContruct (A x) = A x
+lmbContruct (Abs x m) = Abs x (lmbContruct m)
+lmbContruct (App (A x) m) = (App (A x) (lmbContruct m))
+lmbContruct (App (Abs x m) n) = sub m n x
+lmbContruct (App m n)
+  | [] /= getBetaRedex m = App (lmbContruct m) n
+  | otherwise = App m (lmbContruct n)
 
-
-
-
-
-
-m0 = App (ws !! 0) (ws !! 5)
-m1 = Abs (vs !! 0) m0
-m2 = App m1 m0
-
-n0 = ws !! 5
-n1 = ws !! 3
-
--- u = v0, v = v1, w = v2, x = v3, y = v4, z = v5
--- xv(λyz.yv)w = (((v3v1)(λv4.(λv5.(v4v1))))v2)
--- (λy.yx(λx.y(λy.z)x))vw = (((λv4.((v4v3)(λv3.((v4(λv4.v5))v3) )))v1)v2)
-
-m3 = App (ws !! 3) (ws !! 1)
-m4 = App (ws !! 4) (ws !! 1)
-m5 = Abs (vs !! 5) m4
-m6 = Abs (vs !! 4) m5
-m7 = App m3 m6
-m8 = App m7 (ws !! 2)
-
-m9 = Abs (vs !! 4) (ws !! 5)
-m10 = App (ws !! 4) m9
-m11 = App m10 (ws !! 3)
-m12 = Abs (vs !! 3) m11
-m13 = App (ws !! 4) (ws !! 3)
-m14 = App m13 m12
-m15 = Abs (vs !! 4) m14
-m16 = App m15 (ws !! 1)
-m17 = App m16 (ws !! 2)
-
-m18 = App m2 (ws !! 1)
-m19 = App (ws !! 0) m17
-
-p0 = Abs (vs !! 4) (ws !! 3)
-p1 = ws !! 2
--- sub p0 p1 (vs !! 3)
-p2 = Abs (vs !! 2) (ws !! 3)
-p3 = ws !! 2
-
-p4 = App (ws !! 1) (ws !! 2)
-p5 = App p4 (ws !! 3)
-p6 = Abs (vs !! 2) p5
-p7 = App (ws !! 3) p6
-p8 = Abs (vs !! 4) p7
-p9 = App (ws !! 0) (ws !! 1)
--- sub p8 p9 (vs !! 3)
-
-p10 = App (ws !! 3) (ws !! 1)
-p11 = Abs (vs !! 1) p10
-p12 = App (ws !! 4) p11
-
-p13 = App (ws !! 1) (ws !! 4)
-p14 = Abs (vs !! 4) p13
--- sub p12 p14 (vs !! 3)
+-- WARNING!! The following function may cause an infinite loop.
+lmbReduce :: LT -> LT
+lmbReduce m
+  | [] /= getBetaRedex m = lmbReduce (lmbContruct m)
+  | otherwise = m
